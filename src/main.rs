c57c07a59fc9d27147a3d5ddb3fa44c55b0a8f01
Edgar Onghena <dev@edgar.bzh>
2022-05-16 16:48:49 +0200

    Update dependencies, remove useless Mutex and display live question count

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use rocket_contrib::templates::Template;
-use rocket_contrib::serve::StaticFiles;
 use rocket::get;
 use rocket::post;
 use rocket::State;
 use rocket::response::Redirect;
 use rocket::uri;
-use rocket::http::{Cookie, Cookies};
+use rocket::http::{Cookie, CookieJar};
 use std::collections::HashMap;
 use std::sync::Mutex;
+use rocket::fs::FileServer;
+use rocket_dyn_templates::Template;
 use serde::{Deserialize, Serialize};
 use tokio::fs;
 
@@ -37,7 +37,7 @@ macro_rules! map {
         {
             #[allow(unused_mut)]
             let mut hm = HashMap::new();
-            
+
              $(hm.insert(Category::$cat, $score);)*
 
             hm
@@ -61,8 +61,8 @@ struct Session {
     score : HashMap<Category, i32>
 }
 
-#[tokio::main]
-async fn main() {
+#[rocket::launch]
+async fn launch() -> _ {
     // initalisation serde
     let question_string = fs::read_to_string("questions.json").await.unwrap();
     let questions: Vec<Question> = serde_json::from_str(&question_string).unwrap();
@@ -73,7 +73,7 @@ async fn main() {
         }
     }
 
-    rocket::ignite()
+    rocket::build()
         .mount("/", rocket::routes![
             home,
             create_session,
@@ -81,23 +81,21 @@ async fn main() {
             register_answer,
             score,
         ])
-        .mount("/static", StaticFiles::from("static"))
+        .mount("/static", FileServer::from("static"))
         .attach(Template::fairing())
-        .manage(Mutex::new(questions))
+        .manage(questions)
         .manage(Mutex::new(Vec::<Session>::new()))
-        .launch()
-        .await
-        .unwrap();
 }
 
 #[get("/")]
-fn home() -> Template {
-    let truc = HashMap::<i32, i32>::new();
-    Template::render("index", truc)
+fn home(questions: &State<Vec<Question>>) -> Template {
+    Template::render("index", rocket_dyn_templates::context! {
+        question_count: questions.len(),
+    })
 }
 
 #[post("/start")]
-fn create_session(session : State<Mutex<Vec<Session>>>, mut cookies: Cookies) -> Redirect {
+fn create_session(session : &State<Mutex<Vec<Session>>>, cookies: &CookieJar<'_>) -> Redirect {
     let mut session = session.lock().unwrap();
     let score = map!{
         Trashness => 0,
@@ -108,13 +106,12 @@ fn create_session(session : State<Mutex<Vec<Session>>>, mut cookies: Cookies) ->
     let sess_id: u32 = rand::random();
     session.push(Session { cookie : format!("{}", sess_id), score});
     cookies.add(Cookie::new("session", format!("{}", sess_id)));
-    Redirect::to(uri!(question : 0))
+    Redirect::to(uri!(question(0)))
 }
 
 #[get("/<id_question>")]
-fn question(id_question: usize, questions: State<Mutex<Vec<Question>>>) -> Template {
-    let questions_locked = questions.lock().unwrap();
-    let question = &questions_locked[id_question];
+fn question(id_question: usize, questions: &State<Vec<Question>>) -> Template {
+    let question = &questions[id_question];
     Template::render("question", question)
 }
 
@@ -122,30 +119,28 @@ fn question(id_question: usize, questions: State<Mutex<Vec<Question>>>) -> Templ
 fn register_answer(
     id_question: usize,
     id_rep: usize,
-    cookies: Cookies,
-    sessions: State<Mutex<Vec<Session>>>,
-    questions: State<Mutex<Vec<Question>>>
+    cookies: &CookieJar<'_>,
+    sessions: &State<Mutex<Vec<Session>>>,
+    questions: &State<Vec<Question>>,
 ) -> Redirect {
     let session_id = cookies.get("session").unwrap();
     let mut sessions = sessions.lock().unwrap();
     let session = sessions.iter_mut().find(|x| x.cookie == session_id.value()).unwrap();
-    let questions_locked = questions.lock().unwrap();
-    for (category, to_add) in questions_locked[id_question].choices[id_rep].score.clone() {
+    for (category, to_add) in questions[id_question].choices[id_rep].score.clone() {
         *session.score.entry(category).or_insert(0) += to_add;
     }
 
-    if id_question + 1 >= questions_locked.len() {
+    if id_question + 1 >= questions.len() {
         Redirect::to(uri!(score))
     } else {
-        Redirect::to(uri!(question : id_question = id_question + 1))
+        Redirect::to(uri!(question(id_question = id_question + 1)))
     }
 }
 
 #[get("/score")]
-fn score(sessions: State<Mutex<Vec<Session>>>, cookies: Cookies) -> Template {
+fn score(sessions: &State<Mutex<Vec<Session>>>, cookies: &CookieJar<'_>) -> Template {
     let session = get_session!(sessions, cookies);
     let mut template_vars = HashMap::new();
     template_vars.insert("session", session);
     Template::render("score", template_vars)
 }
-
